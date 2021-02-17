f65940865a2d5fb37767bad0f58d02856aca1a85
Steven Brown <beren1hand@steveAir.local>
2021-02-17 13:58:09 -0500

    Made it look in the build directory

--- herokutest/herokutest/settings.py
+++ herokutest/herokutest/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BUILD_DIR = os.path.join(BASE_DIR.parent,"build")
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'herokutest.herokutest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BUILD_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
